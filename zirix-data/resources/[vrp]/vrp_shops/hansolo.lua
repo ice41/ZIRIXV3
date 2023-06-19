f26059a81f053fa95e34a594ac2438dfd92ac9a5
ice41 <edu.ice7@gmail.com>
2023-06-19 18:54:34 +0100

    Update hansolo.lua

--- zirix-data/resources/[vrp]/vrp_shops/hansolo.lua
+++ zirix-data/resources/[vrp]/vrp_shops/hansolo.lua
@@ -90,10 +90,40 @@ Citizen.CreateThread(function()
 				local distance = GetDistanceBetweenCoords(v.x, v.y, cdz, x, y, z, true)
 				if distance < 5.0 then
 					idle = 5
-					DrawMarker(23, v.x, v.y, v.z-0.97,0,0,0,0,0,0,0.7,0.7,0.5, 66, 236, 134, 150,0,0,0,0)
+					--DrawMarker(23, v.x, v.y, v.z-0.97,0,0,0,0,0,0,0.7,0.7,0.5, 66, 236, 134, 150,0,0,0,0)
+					DrawText3D(v.x, v.y, v.z-0.05, "[~g~E~w~] Para abrir a  ~g~Loja~w~.")
 				end
 			end
 		end
 		Citizen.Wait(idle)
 	end
 end)
+
+--[ FUNÇÕES ]----------------------------------------------------------------------------------------------------------------------------
+
+function DrawText3D(x,y,z, text)
+    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+    
+    SetTextScale(0.28, 0.28)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(text)
+    DrawText(_x,_y)
+    local factor = (string.len(text)) / 370
+    DrawRect(_x,_y+0.0125, 0.005+ factor, 0.03, 41, 11, 41, 68)
+end
+
+function drawTxt(text,font,x,y,scale,r,g,b,a)
+	SetTextFont(font)
+	SetTextScale(scale,scale)
+	SetTextColour(r,g,b,a)
+	SetTextOutline()
+	SetTextCentre(1)
+	SetTextEntry("STRING")
+	AddTextComponentString(text)
+	DrawText(x,y)
+end
