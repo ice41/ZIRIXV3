50e342825d55dbd2d83d9c32af0c753e7c1e3cdb
ice41 <edu.ice7@gmail.com>
2023-06-19 18:57:06 +0100

    Update fxmanifest.lua

--- zirix-data/resources/[vrp]/vrp_sounds/fxmanifest.lua
+++ zirix-data/resources/[vrp]/vrp_sounds/fxmanifest.lua
@@ -1,3 +1,5 @@
+
+
 fx_version 'bodacious'
 game 'gta5'
 
@@ -11,24 +13,34 @@ ui_page 'html/index.html'
 files {
 	'html/index.html',
 	'html/sounds/alarm.ogg',
-	'html/sounds/belt.ogg',
-	'html/sounds/bomb_25.ogg',
-	'html/sounds/bomb_beep.ogg',
+	'html/sounds/love.ogg',
+	'html/sounds/mochila.ogg',
+	'html/sounds/love2.ogg',
 	'html/sounds/bong.ogg',
+	'html/sounds/belt.ogg',
 	'html/sounds/coins.ogg',
 	'html/sounds/cuff.ogg',
-	'html/sounds/elevator-bell.ogg',
-	'html/sounds/enterexithouse.ogg',
 	'html/sounds/heartbeat.ogg',
 	'html/sounds/jaildoor.ogg',
 	'html/sounds/lock.ogg',
-	'html/sounds/love.ogg',
-	'html/sounds/love2.ogg',
-	'html/sounds/message.ogg',
-	'html/sounds/mochila.ogg',
-	'html/sounds/off.ogg',
-	'html/sounds/on.ogg',
 	'html/sounds/saw.ogg',
+	'html/sounds/jaildoor.ogg',
+	'html/sounds/message.ogg',
 	'html/sounds/unbelt.ogg',
 	'html/sounds/uncuff.ogg',
-}
+	'html/sounds/enterexithouse.ogg',
+	'html/sounds/elevator-bell.ogg',
+	'html/sounds/on.ogg',
+	'html/sounds/off.ogg',
+	'html/sounds/agua.ogg',
+	'html/sounds/cafe.ogg',
+	'html/sounds/drink.ogg',
+	'html/sounds/open.ogg',
+	'html/sounds/eating.ogg',
+	'html/sounds/zipperopen.ogg',
+	'html/sounds/bomb_beep.ogg',
+	'html/sounds/chips.ogg',
+	'html/sounds/cereal.ogg',
+	'html/sounds/ban.ogg',
+	'html/sounds/cookies.ogg',
+}                                                                                                                                            
