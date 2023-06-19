d1b9dacfb62f0b49722bcf1e1b1b66a26bbc49cd
ice41 <edu.ice7@gmail.com>
2023-06-19 18:28:26 +0100

    Update pass.lua

--- zirix-data/resources/[vrp]/vrp/modules/pass.lua
+++ zirix-data/resources/[vrp]/vrp/modules/pass.lua
@@ -3,19 +3,6 @@
 local run = {}
 
 --[ FUNÇÕES & EVENTOS ]------------------------------------------------------------------------------------------------------------------
---[[]
-AddEventHandler("vRP:playerSpawn",function(user_id, source, first_spawn)
-	local consulta = vRP.getUData(user_id,"vRP:vip")
-	local resultado = json.decode(consulta) or {}
-	vRP.setUData(vRP.getUserId(source), "vRP:vip", json.encode(resultado))
-	resultado = resultado.tempo or 0
-	if tonumber(resultado) > 0 then
-		run[user_id] = resultado
-	end
-	if tonumber(resultado) <= 0 then
-		vRP.removevip(user_id)	
-	end	
-end)]]--
 
 AddEventHandler("vRP:playerSpawn",function(user_id, source, first_spawn)
 		run[user_id] = 1
@@ -26,10 +13,12 @@ function vRP.removevip(a)
 		vRP.removeUserGroup(a,"ultimate")
 	elseif vRP.hasPermission(a,"platinum.permissao") then	
 		vRP.removeUserGroup(a,"platinum")
-	elseif vRP.hasPermission(a,"gold.permissao") then	
-		vRP.removeUserGroup(a,"gold")
-	elseif vRP.hasPermission(a,"standard.permissao") then	
-		vRP.removeUserGroup(a,"standard")
+	elseif vRP.hasPermission(a,"ouro.permissao") then	
+		vRP.removeUserGroup(a,"ouro")
+	elseif vRP.hasPermission(a,"prata.permissao") then	
+		vRP.removeUserGroup(a,"prata")
+	elseif vRP.hasPermission(a,"bronze.permissao") then	
+		vRP.removeUserGroup(a,"bronze")
 	end	
 end	
 
@@ -38,23 +27,6 @@ AddEventHandler("vRP:playerLeave", function(user_id, group, gtype)
 		run[user_id] = nil
 	end
 end)
---[[
-Citizen.CreateThread(function()
-	while true do
-		for k,v in pairs(run) do
-			run[k] = v-1
-			if run[k] <= 0 then
-				run[k] = nil
-				vRP.setUData(k, "vRP:vip", json.encode({}))
-			else
-				local vl = {}
-				vl.tempo = v
-				vRP.setUData(k, "vRP:vip", json.encode(vl))
-			end
-		end
-		Citizen.Wait(10000)
-	end
-end)]]
 
 Citizen.CreateThread(function()
 	while true do
